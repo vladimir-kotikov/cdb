1652cf3c4ab3dc542eda8a5311cd7f5883c118ff
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2025-01-06 00:23:23 +0200

    Add shell command to launch debug

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
+import { exec } from "child_process";
 import path from "path";
 import { URLSearchParams } from "url";
+import { promisify } from "util";
 import * as vscode from "vscode";
 import { adapters } from "./adapters";
 import { mapFirst } from "./array_utils";
 
-interface DebugParams {
+interface CdbParams {
   cwd?: string;
   program: string;
   [key: string]: string | string[] | undefined;
@@ -12,11 +14,18 @@ interface DebugParams {
 
 const isSubpath = (current: string, maybeParent: string): boolean => {
   const relative = path.relative(maybeParent, current);
-  return !!relative && !relative.startsWith("..") && !path.isAbsolute(relative);
+  return !relative || !relative.startsWith("..") && !path.isAbsolute(relative);
+};
+
+const installCdb = async (sourceDir: string) => {
+  const source = `/usr/local/bin/cdb`;
+  const target = path.join(sourceDir, "cdb");
+  const command = `osascript -e "do shell script \\"mkdir -p /usr/local/bin && ln -sf \'${target}\' \'${source}\'\\" with administrator privileges"`;
+  await promisify(exec)(command);
 };
 
 class DebugUriHandler implements vscode.UriHandler {
-  private parseUri(uri: vscode.Uri): [string, DebugParams] {
+  private parseUri(uri: vscode.Uri): [string, CdbParams] {
     const action = uri.path;
     const urlParams = new URLSearchParams(uri.query);
 
@@ -37,23 +46,28 @@ class DebugUriHandler implements vscode.UriHandler {
     return [action, { ...debugParams, program: debugParams.program }];
   }
 
-  private handleDebug(debugParams: DebugParams): vscode.ProviderResult<void> {
-    const workspaceFolder = !!debugParams.cwd ? vscode.workspace.workspaceFolders?.find((f) =>
-      isSubpath(debugParams.cwd!, f.uri.fsPath),
-    ) : undefined;
+  private handleDebug(cdbParams: CdbParams): vscode.ProviderResult<any> {
+    const { cwd, program } = cdbParams;
+    const workspaceFolder = !!cwd
+      ? vscode.workspace.workspaceFolders?.find((f) =>
+        isSubpath(cwd!, f.uri.fsPath),
+      )
+      : undefined;
 
     // TODO: More robust args parsing
-    const argv = debugParams.program.split(" ");
-    const debugAdapterParams = mapFirst(adapters, adapter => adapter(argv));
-
-    if (!debugAdapterParams) {
-      return vscode.window.showErrorMessage(
-        `Could not start debug session: unknown program ${debugParams.program}`,
-      ).then();
+    const debugParams = mapFirst(adapters, (adapter) =>
+      adapter(program.split(" "), cwd),
+    );
+    if (!debugParams) {
+      return vscode.window
+        .showErrorMessage(
+          `Could not start debug session: unknown program ${program}`,
+        )
+        .then();
     }
 
-    vscode.debug.startDebugging(workspaceFolder, {
-      name: `cdb: ${debugParams.program}`,
+    return vscode.debug.startDebugging(workspaceFolder, {
+      name: `cdb: ${program}`,
       request: "launch",
       justMyCode: false,
       ...debugParams,
@@ -76,8 +90,10 @@ class DebugUriHandler implements vscode.UriHandler {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.window.registerUriHandler(new DebugUriHandler());
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.window.registerUriHandler(new DebugUriHandler()),
+    vscode.commands.registerCommand("cdb.install", () => installCdb(context.extensionPath)),
+  );
 }
 
 export function deactivate() { }
